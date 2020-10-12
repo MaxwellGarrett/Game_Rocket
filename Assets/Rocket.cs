540c0f9556788ae957147b325ad1ddeee5e655c6
MaxwellGarrett <44849328+MaxwellGarrett@users.noreply.github.com>
2020-10-12 14:30:57 -0500

    sound and movement

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour{
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -16,15 +19,28 @@ public class Rocket : MonoBehaviour{
 
     private void ProcessInput(){
         if(Input.GetKey(KeyCode.Space)){
-            print("thrust");
+            // adds force to make rocket go up
+            rigidBody.AddRelativeForce(Vector3.up);
+
+            // so audio doesnt play
+            if(! audioSource.isPlaying){
+                audioSource.Play();
+            }
+        }
+
+        else{
+            audioSource.Stop();
         }
         
+        // rotate the ship left or right
         if(Input.GetKey(KeyCode.A)){
-            print("rotate left");
+            // print("rotate left");
+            transform.Rotate(Vector3.forward);
         }
         
         else if(Input.GetKey(KeyCode.D)){
-            print("rotate right");
+            // print("rotate right");
+             transform.Rotate(-Vector3.forward);
         }
     }
 }
