a9fa21923aa665a5563dc6a8288329a55224ee1c
MaxwellGarrett <44849328+MaxwellGarrett@users.noreply.github.com>
2020-11-12 11:50:49 -0600

    new level

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour{
-    float rcsThrust = 100f;
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 100f;
     
     Rigidbody rigidBody;
     AudioSource audioSource;
@@ -47,13 +48,33 @@ public class Rocket : MonoBehaviour{
         // resume physics control  
         rigidBody. freezeRotation = false;
     }
+
+    void OnCollisionEnter(Collision collision){
+        print("collisoin");
+        switch (collision.gameObject.tag) {
+            case "Friendly":
+               print("Excellent!");
+               break;
+
+            default:
+                print("Deadly");
+                break; 
+        }
+    }
+
     private void Thrust(){
+        if (Input.GetKey(KeyCode.Space)){
             // adds force to make rocket go up
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
 
             // so audio doesnt play
             if(! audioSource.isPlaying){
                 audioSource.Play();
+            }
+
+            else {
+                audioSource.Stop();
             }
         }
 }
